98ee42eff70db7328bd5f84834dd68297fce0529
“Sabrian <sabrian.salim@gmail.com>
2022-04-02 13:39:36 +1300

    update code to be flexible

--- main.py
+++ main.py
@@ -25,90 +25,90 @@ class PDFForm(FlaskForm):
 def main():
     form = PDFForm()
     if form.validate_on_submit():
-        try:
-            pdf = form.chooseFile.data
 
-            with fitz.open("pdf", pdf.read()) as doc:
-                global text
-                text = ""
-                for page in doc:
-                    text += page.getText()
+        pdf = form.chooseFile.data
 
-            data = text.split("\n")
+        with fitz.open("pdf", pdf.read()) as doc:
+            global text
+            text = ""
+            for page in doc:
+                text += page.getText()
 
-            indexs = [1, 4]
-
-            accommodation_total = []
-            upgrade_total = []
-            parking_total = []
-            restaurant_total = []
-            income_total = []
-
-            for index in indexs:
-                accommodation = []
-                early_check_in = accommodation.append(
-                    float(data[data.index("Accom early check in") + index].replace(',', '')))
-                accom_revenue = accommodation.append(
-                    float(data[data.index("Accommodation Revenue") + index].replace(',', '')))
-                credit_card = accommodation.append(float(data[data.index("Credit card fees") + index].replace(',', '')))
-                dry_cleaning = accommodation.append(
-                    float(data[data.index("Drycleaning / Laundry Charges") + index].replace(',', '')))
+        data = text.split("\n")
 
 
 
-                phone_charges = accommodation.append(float(data[data.index("Phone Charges") + index].replace(',', '')))
+        indexs = [1, 4]
 
-                sundry = accommodation.append(float(data[data.index("Sundry Income") + index].replace(',', '')))
+        accommodation_total = []
+        upgrade_total = []
+        parking_total = []
+        restaurant_total = []
+        income_total = []
 
-                total = 0
+        for index in indexs:
+            accommodation = []
 
-                for val in accommodation:
-                    total += val
-                accommodation_total.append(round(total, 2))
+            ACCOMS = ["Accom early check in", "Accom late check out", "Accommodation Revenue",
+                     "Accommodation-No Show", "Courier-on guest behalf",
+                     "Credit card fees", "Drycleaning / Laundry Charges", "Guest Attrition Fees",
+                     "Guest Cancellation Fees", "Hotel Shop Revenue",
+                     "Housekeeping Service", "Internet Charges", "Phone Charges", "Sofa Bed", "Sundry Income"]
 
-                upgrade = float(data[data.index("Accommodation upgrade") + index].replace(',', ''))
-                upgrade_total.append(upgrade)
+            for accom_data in ACCOMS:
+                try:
+                    accommodation.append(
+                        float(data[data.index(accom_data) + index].replace(',', '')))
+                except ValueError:
+                    accommodation.append(0)
 
-                parking = float(data[data.index("Car Park Charges") + index].replace(',', ''))
-                parking_total.append(parking)
+            total = 0
 
-                restaurant = []
+            for val in accommodation:
+                total += val
+            accommodation_total.append(round(total, 2))
 
-                all_day_beverage = restaurant.append(
-                    float(data[data.index("Restaurant All Day Beverage") + index].replace(',', '')))
-                all_day_food = restaurant.append(
-                    float(data[data.index("Restaurant All Day Food") + index].replace(',', '')))
+            upgrade = float(data[data.index("Accommodation upgrade") + index].replace(',', ''))
 
-                bf_food = restaurant.append(
-                    float(data[data.index("Restaurant Breakfast Food") + index].replace(',', '')))
-                dinner_beverage = restaurant.append(
-                    float(data[data.index("Restaurant Dinner Beverage") + index].replace(',', '')))
-                dinner_food = restaurant.append(
-                    float(data[data.index("Restaurant Dinner Food") + index].replace(',', '')))
+            upgrade_total.append(upgrade)
 
-                lunch_food = restaurant.append(
-                    float(data[data.index("Restaurant Lunch Food") + index].replace(',', '')))
+            parking = float(data[data.index("Car Park Charges") + index].replace(',', ''))
+            parking_total.append(parking)
 
+            restaurant = []
 
-                total = 0
+            RESTAURANTS = ["Child Full cooked breakfast", "Restaurant All Day Beverage", "Restaurant All Day Food",
+                    "Restaurant Box breakfast", "Restaurant Breakfast Food", "Restaurant Dinner Beverage",
+                    "Restaurant Dinner Food", "Restaurant Lunch Beverage", "Restaurant Lunch Food",
+                    "Room Service Breakfast - Beverage", "Vending Machine Revenue-Drinks+snacks", "Vie Venue Hire"]
 
-                for val in restaurant:
-                    total += val
-                restaurant_total.append(round(total, 2))
+            for restaurant_data in RESTAURANTS:
+                try:
+                    restaurant.append(
+                        float(data[data.index(restaurant_data) + index].replace(',', '')))
+                except ValueError:
+                    accommodation.append(0)
 
-                income = income_total.append(float(data[data.index("Total Income") + index].replace(',', '')))
+            total = 0
 
-            if round(accommodation_total[0] + upgrade_total[0] + parking_total[0] + restaurant_total[0], 2) == income_total[0] and \
-                    round(accommodation_total[1] + upgrade_total[1] + parking_total[1] + restaurant_total[1], 2) == income_total[1]:
-                titles = ["Daily", "Monthly"]
-                return render_template("index.html", form=form, accommodation_total=accommodation_total,
-                                       upgrade_total=upgrade_total, parking_total=parking_total,
-                                       restaurant_total=restaurant_total, income_total=income_total,
-                                       titles=titles, year=datetime.date.today().year)
-            else:
-                return "<h1>Something is wrong</h1>"
-        except ValueError:
-            return "<h1>Only flash report is allowed</h1>"
+            for val in restaurant:
+                total += val
+            restaurant_total.append(round(total, 2))
+
+            income = income_total.append(float(data[data.index("Total Income") + index].replace(',', '')))
+
+        if round(accommodation_total[0] + upgrade_total[0] + parking_total[0] + restaurant_total[0], 2) == \
+                income_total[0] and \
+                round(accommodation_total[1] + upgrade_total[1] + parking_total[1] + restaurant_total[1], 2) == \
+                income_total[1]:
+            titles = ["Daily", "Monthly"]
+            return render_template("index.html", form=form, accommodation_total=accommodation_total,
+                                   upgrade_total=upgrade_total, parking_total=parking_total,
+                                   restaurant_total=restaurant_total, income_total=income_total,
+                                   titles=titles, year=datetime.date.today().year)
+        else:
+            return "<h1>Something is wrong</h1>"
+
     else:
         errors = form.chooseFile.errors
         return render_template("index.html", form=form, errors=errors, year=datetime.date.today().year)
